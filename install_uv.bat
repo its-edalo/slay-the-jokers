e3c232a2d55305b62e3ac8c7b7c95415bc9ba39e
its-edalo <itsedalo@gmail.com>
2025-03-15 16:25:14 +0200

    Switched to double quotes when running uvx command
    Using single quotes failed when running the script from commandline

--- install_uv.bat
+++ install_uv.bat
@@ -3,5 +3,5 @@ if not exist "%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe" (
     set UV_UNMANAGED_INSTALL=%APPDATA%\Balatro\Mods\SlayTheJokers
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 )
-%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c 'import psutil, google.cloud'
-echo All required Python libraries have been installed.
+%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c "import psutil, google.cloud"
+if %ERRORLEVEL%==0 echo All required Python libraries have been installed.
