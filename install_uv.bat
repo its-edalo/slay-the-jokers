1f5b8735104f34fb55c41d072de64a356829198e
its-edalo <itsedalo@gmail.com>
2025-03-28 01:19:47 +0200

    Deleted install_uv.bat

--- install_uv.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist "%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe" (
-    set UV_UNMANAGED_INSTALL=%APPDATA%\Balatro\Mods\SlayTheJokers
-    powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
-)
-%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c "import psutil, google.cloud"
-if %ERRORLEVEL%==0 echo All required Python libraries have been installed.
