f252cc9b822dc892d0a221992af9dc37562dad2c
its-edalo <itsedalo@gmail.com>
2025-04-01 01:08:50 +0300

    Powershell `uv` installer (#19)
    Added a powershell script to install `uv` that will run automatically, replacing the old .bat

---
+++ stj_launch_uploader.ps1
@@ -0,0 +1,9 @@
+$modPath = "$env:APPDATA\Balatro\Mods\SlayTheJokers"
+$uvxPath = Join-Path $modPath "uvx.exe"
+
+if (-Not (Test-Path $uvxPath)) {
+    $env:UV_UNMANAGED_INSTALL = $modPath
+    irm https://astral.sh/uv/install.ps1 | iex
+}
+
+& $uvxPath --with psutil --with requests python@3.12 "$modPath\stj_uploader.py"
