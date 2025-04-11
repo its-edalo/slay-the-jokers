395985d7d71ee12729373cb0269f1676359d0f04
its-edalo <itsedalo@gmail.com>
2025-04-11 12:24:22 +0300

    Added a steamodded metadata file and icon

--- src/stj_save.lua
+++ src/stj_save.lua
@@ -1,4 +1,4 @@
-STJ_VERSION = "0.1.3"
+STJ_VERSION = "0.1.3a"
 
 function is_encodable(card)
     if not card.ability or not card.ability.name or not card.ability.set then
---
+++ steamodded.lua
@@ -0,0 +1,8 @@
+if SMODS and SMODS.current_mod then
+	SMODS.Atlas({
+		key = "modicon",
+		path = "icon.png",
+		px = 32,
+		py = 32,
+	})
+end
