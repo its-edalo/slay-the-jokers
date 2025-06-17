c1e4ebd7ef30bd7986f75e3092bfcdb817737cff
its-edalo <itsedalo@gmail.com>
2025-06-17 19:37:03 +0300

    Feature/v0.2 (#30)
    Encode Stickers and Editions

--- src/stj_save.lua
+++ src/stj_save.lua
@@ -1,4 +1,5 @@
-STJ_VERSION = "0.1.3a"
+STJ_VERSION = "0.2"
+MAX_ENCODED_CARDS_PER_SOURCE = 50
 
 function is_encodable(card)
     if not card.ability or not card.ability.name or not card.ability.set then
@@ -20,41 +21,111 @@ function is_encodable(card)
     return true
 end
 
+function get_card_stickers(card)
+    local stickers = {}
+
+    -- perishanle
+    if (card.ability.perishable) then
+        stickers["p"] = card.ability.perish_tally
+    end
+
+    -- rental
+    if (card.ability.rental) then
+        stickers["r"] = 1
+    end
+
+    -- eternal
+    if (card.ability.eternal) then
+        stickers["e"] = 1
+    end
+
+    return stickers
+end
+
+function get_card_edition(card)
+    if not card.edition then
+        return nil
+    end
+
+    if card.edition.holo then
+        return "h"
+    elseif card.edition.foil then
+        return "f"
+    elseif card.edition.polychrome then
+        return "p"
+    elseif card.edition.negative then
+        if card.ability.consumeable then
+            return "nc"
+        else
+            return "n"
+        end
+    end
+
+    return nil
+end
+
 function encode_card(card)
     local data = {}
-    local is_modded = false
-    local desc_args = nil
+    local name = nil
 
     if not is_encodable(card) then
         return nil
     end
 
-    local name = card.ability.name
-    if card:is_modded() then
-        is_modded = true
-        local localization_set = G.localization.descriptions[card.ability.set]
-        if localization_set and card.config.center_key then
-            local localization_desc = localization_set[card.config.center_key]
-            if localization_desc and localization_desc.name then
-                name = localization_desc.name
-            end
-        end
-    end
-
-    if name == "Riff-raff" then
-        name = "Riff-Raff"
-    elseif name == "Caino" then
-        name = "Canio"
-    end
-
     if card.facing and card.facing =='back' then
         name = "?"
     else
-        desc_args = card:get_description_table(is_modded)
+        name = card.ability.name
 
-        -- perishanle
-        if (card.ability.perishable) then
-            data.s = {["p"] = card.ability.perish_tally}
+        if name == "Riff-raff" then
+            name = "Riff-Raff"
+        elseif name == "Caino" then
+            name = "Canio"
+        end
+
+        local stickers = get_card_stickers(card)
+
+        if stickers and next(stickers) then
+            -- stickers
+            data.s = stickers
+        end
+
+        local edition = get_card_edition(card)
+        if edition then
+            -- edition
+            data.e = edition
+        end
+
+        if (card:is_modded()) then
+            -- modded
+            data.m = 1
+
+            local localization_set = G.localization.descriptions[card.ability.set]
+            if localization_set and card.config.center_key then
+                local localization_desc = localization_set[card.config.center_key]
+                if localization_desc and localization_desc.name then
+                    name = localization_desc.name
+                end
+            end
+
+            if card.ability.set == "Joker" and card.config.center.rarity then
+                local rarity = card.config.center.rarity
+
+                local card_types = {"Common", "Uncommon", "Rare", "Legendary"}
+                if (card_types[rarity]) then
+                    data.r = card_types[rarity]
+                else
+                    local localized_rarity = localize("k_" .. rarity:lower())
+                    if (localized_rarity ~= 'ERROR') then
+                        data.r = localized_rarity
+                    end
+                end
+            end
+            -- description
+            data.d = card:get_description_table(true)
+        else
+            -- description
+            data.d = card:get_description_table(false)
         end
     end
 
@@ -69,29 +140,45 @@ function encode_card(card)
     -- popup direction
     data.p = card:get_popup_direction()
 
-    -- description
-    data.d = desc_args
+    return data
+end
 
-    -- modded
-    if (is_modded) then
-        data.m = 1
+function can_get_run_info()
+    return G.GAME and G.GAME.hands
+end
 
-        if card.ability.set == "Joker" and card.config.center.rarity then
-            local rarity = card.config.center.rarity
+function get_run_info()
+    local run_info = {}
+    local possible_hands = {
+        "High Card",
+        "Pair",
+        "Two Pair",
+        "Three of a Kind",
+        "Straight",
+        "Flush",
+        "Full House",
+        "Four of a Kind",
+        "Straight Flush",
+        "Five of a Kind",
+        "Flush House",
+        "Flush Five"
+    }
 
-            local card_types = {"Common", "Uncommon", "Rare", "Legendary"}
-            if (card_types[rarity]) then
-                data.r = card_types[rarity]
-            else
-                local localized_rarity = localize("k_" .. rarity:lower())
-                if (localized_rarity ~= 'ERROR') then
-                    data.r = localized_rarity
-                end
-            end
+    run_info.h = {}
+
+    for index, handname in ipairs(possible_hands) do
+        local hand = G.GAME.hands[handname]
+        if hand and hand.visible then
+            run_info.h[index] = {
+                ["l"] = hand.level,
+                ["c"] = hand.chips,
+                ["m"] = hand.mult,
+                ["p"] = hand.played
+            }
         end
     end
 
-    return data
+    return run_info
 end
 
 function Game:stj_save()
@@ -117,11 +204,16 @@ function Game:stj_save()
 
         for _, source in pairs(card_sources) do
             if source and source.cards then
+                local encoded_count = 0
                 for _, v in pairs(source.cards) do
+                    if encoded_count >= MAX_ENCODED_CARDS_PER_SOURCE then
+                        break
+                    end
                     if v.ability and not unsaved_card_sets[v.ability.set] then
                         local encoded_card = encode_card(v)
                         if encoded_card then
                             table.insert(card_data, encoded_card)
+                            encoded_count = encoded_count + 1
                         end
                     end
                 end
@@ -130,6 +222,13 @@ function Game:stj_save()
 
         live_data.v = STJ_VERSION
         live_data.c = card_data
+
+        -- if can_get_run_info() then
+        --     local run_info = get_run_info()
+        --     if run_info then
+        --         live_data.r = run_info
+        --     end
+        -- end
 
         G.STJ_MANAGER.channel:push({
             type = 'save_stj_data',
